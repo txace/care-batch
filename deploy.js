3c41c660cd9e8459550a7af8bf49b1cfd4b5b82e
Tim Markello <timmarkello@gmail.com>
2015-08-18 15:31:42 -0500

    + Standalone dx-batch update    > script, configuration and deployment of new 'standalone' process for       generating the body of a diagnosis batch    + designed to work in conjunction with another process that is resoponsible for generating the other batch transactions       this one accepts parameters through a parmfile or option string to       append the dx batch transactions to a started batchfile

--- deploy.js
+++ deploy.js
@@ -14,9 +14,18 @@ var options = {
    },
    usc_path: {
       local: './uScript/'
+   },
+   parm_path: {
+      local: './build/'
    }
 };
 
 var mis = util(options);
 
+mis.parm.fromflatfile('./parm/DXCBAT.parm')
+.then(mis.parm.tofile.bind(mis, './build/DXCBAT.parm'))
+.then(mis.deploy.parm)
+.then(mis.script.runonce.bind(mis, './usc/dx-batch.usc'));
+
 mis.script.install('./usc/care-submit.usc');
+//mis.script.runonce('./usc/dx-batch.usc');
